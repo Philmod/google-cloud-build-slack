ef19cbc082b3a4efbdf67c04c32b5ca2416490df
Philmod <philippe.modard@gmail.com>
2017-03-20 14:10:27 -0400

    fix setup script for ubuntu

--- setup.sh
+++ setup.sh
@@ -8,11 +8,20 @@ if [ -z "$GC_SLACK_STATUS" ]; then
   export GC_SLACK_STATUS="SUCCESS FAILURE TIMEOUT INTERNAL_ERROR"
 fi
 arr=(`echo ${GC_SLACK_STATUS}`);
-json_arr=$(printf '%s\n' "${arr[@]}" | jq -R . | jq -s .)
+json_array() {
+  echo -n '['
+  while [ $# -gt 0 ]; do
+    x=${1//\\/\\\\}
+    echo -n \"${x//\"/\\\"}\"
+    [ $# -gt 1 ] && echo -n ', '
+    shift
+  done
+  echo ']'
+}
 cat <<EOF > config.json
 {
   "SLACK_WEBHOOK_URL" : "$SLACK_WEBHOOK_URL",
-  "GC_SLACK_STATUS": $json_arr
+  "GC_SLACK_STATUS": $(json_array "${arr[@]}")
 }
 EOF
 
@@ -21,7 +30,8 @@ if [ -z "$BUCKET_NAME" ]; then
   # Create pseudo random bucket name, otherwise an attacker
   # could use the pattern to create a bucket and get the
   # function code.
-  export BUCKET_NAME="$PROJECT_ID-gcf-$(date | md5 | cut -c -10)"
+  md5=md5 && [[ -n "$(which md5)" ]] || md5=md5sum
+  export BUCKET_NAME="$PROJECT_ID-gcf-$(date | $md5 | cut -c -10)"
 fi
 
 # Create bucket.
