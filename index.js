9e27761be75cadf5f601bfe313d96a03ef95502f
Naseem Ullah <naseemkullah@gmail.com>
2019-02-01 14:50:14 -0500

    Send notification when build is started

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports.subscribe = (event, callback) => {
   const build = module.exports.eventToBuild(event.data.data);
 
   // Skip if the current status is not in the status list.
-  const status = module.exports.status || ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
+  const status = module.exports.status || ['WORKING', 'SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
   if (status.indexOf(build.status) === -1) {
     return callback();
   }
@@ -24,65 +24,81 @@ module.exports.subscribe = (event, callback) => {
 };
 
 // eventToBuild transforms pubsub event message to a build object.
-module.exports.eventToBuild = (data) => {
-  return JSON.parse(new Buffer(data, 'base64').toString());
-}
+module.exports.eventToBuild = (data) => JSON.parse(new Buffer(data, 'base64').toString());
 
 const DEFAULT_COLOR = '#4285F4'; // blue
 const STATUS_COLOR = {
-  'QUEUED': DEFAULT_COLOR,
-  'WORKING': DEFAULT_COLOR,
-  'SUCCESS': '#34A853', // green
-  'FAILURE': '#EA4335', // red
-  'TIMEOUT': '#FBBC05', // yellow
-  'INTERNAL_ERROR': '#EA4335', // red
+  QUEUED: DEFAULT_COLOR,
+  WORKING: DEFAULT_COLOR,
+  SUCCESS: '#34A853', // green
+  FAILURE: '#EA4335', // red
+  TIMEOUT: '#FBBC05', // yellow
+  INTERNAL_ERROR: '#EA4335', // red
 };
 
 // createSlackMessage create a message from a build object.
 module.exports.createSlackMessage = (build) => {
-  let message = {
-    text: `Build \`${build.id}\` finished`,
+  const buildFinishTime = new Date(build.finishTime);
+  const buildStartTime = new Date(build.startTime);
+
+  const timestamp = Math.round(buildFinishTime.getTime() / 1000);
+  const isWorking = build.status === 'WORKING';
+
+  const text = (isWorking)
+    ? `Build \`${build.id}\` started`
+    : `Build \`${build.id}\` finished`;
+
+  const fields = [{
+    title: 'Status',
+    value: build.status,
+  }];
+
+  if (!isWorking) {
+    const buildTime = humanizeDuration(buildFinishTime - buildStartTime);
+
+    fields.push({
+      title: 'Duration',
+      value: buildTime,
+    });
+  }
+
+  const message = {
+    text,
     mrkdwn: true,
     attachments: [
       {
         color: STATUS_COLOR[build.status] || DEFAULT_COLOR,
         title: 'Build logs',
         title_link: build.logUrl,
-        fields: [{
-          title: 'Status',
-          value: build.status
-        }, {
-          title: 'Duration',
-          value: humanizeDuration(new Date(build.finishTime) - new Date(build.startTime))
-        }],
+        fields,
         footer: 'Google Cloud Container Builder',
         footer_icon: 'https://ssl.gstatic.com/pantheon/images/containerregistry/container_registry_color.png',
-        ts: Math.round(new Date(build.finishTime).getTime()/1000)
-      }
-    ]
+        ts: timestamp,
+      },
+    ],
   };
 
   // Add source information to the message.
-  let source = build.source || null;
+  const source = build.source || null;
   if (source) {
     message.attachments[0].fields.push({
       title: 'Repository',
-      value: build.source.repoSource.repoName
+      value: build.source.repoSource.repoName,
     });
 
     message.attachments[0].fields.push({
       title: 'Branch',
-      value: build.source.repoSource.branchName
+      value: build.source.repoSource.branchName,
     });
   }
 
   // Add images to the message.
-  let images = build.images || [];
+  const images = build.images || [];
   for (let i = 0, len = images.length; i < len; i++) {
     message.attachments[0].fields.push({
       title: 'Image',
-      value: images[i]
+      value: images[i],
     });
   }
-  return message
-}
+  return message;
+};
