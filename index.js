ddd16b7b6775c305bd44c77951a6fae1682f1583
Naseem Ullah <naseemkullah@gmail.com>
2019-03-16 01:52:47 -0400

    print commit authors in msg if github token is defined

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const IncomingWebhook = require('@slack/client').IncomingWebhook;
+const { IncomingWebhook } = require('@slack/client');
 const humanizeDuration = require('humanize-duration');
 const config = require('./config.json');
+
+const token = process.env.GITHUB_TOKEN;
+const { getCommitAuthor } = require('./lib/getCommitAuthor');
 
 module.exports.webhook = new IncomingWebhook(config.SLACK_WEBHOOK_URL);
 module.exports.status = config.GC_SLACK_STATUS;
 
 // subscribe is the main function called by GCF.
-module.exports.subscribe = (event, callback) => {
-  const build = module.exports.eventToBuild(event.data.data);
+module.exports.subscribe = async (event) => {
+  const build = module.exports.eventToBuild(event.data);
 
   // Skip if the current status is not in the status list.
   const status = module.exports.status || ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
   if (status.indexOf(build.status) === -1) {
-    return callback();
+    return;
   }
 
   // Send message to slack.
-  const message = module.exports.createSlackMessage(build);
-  module.exports.webhook.send(message, (err, res) => {
-    if (err) console.log('Error:', err);
-    callback(err);
+  const message = await module.exports.createSlackMessage(build);
+  module.exports.webhook.send(message, async (err, res) => {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      console.log('Message sent: ', res);
+    }
   });
 };
 
 // eventToBuild transforms pubsub event message to a build object.
-module.exports.eventToBuild = data => JSON.parse(new Buffer(data, 'base64').toString());
+module.exports.eventToBuild = data => JSON.parse(Buffer.from(data, 'base64').toString());
 
 const DEFAULT_COLOR = '#4285F4'; // blue
 const STATUS_COLOR = {
@@ -37,12 +43,16 @@ const STATUS_COLOR = {
 };
 
 // createSlackMessage create a message from a build object.
-module.exports.createSlackMessage = (build) => {
+module.exports.createSlackMessage = async (build) => {
   const buildFinishTime = new Date(build.finishTime);
   const buildStartTime = new Date(build.startTime);
 
   const isWorking = build.status === 'WORKING';
   const timestamp = Math.round(((isWorking) ? buildStartTime : buildFinishTime).getTime() / 1000);
+
+  const commitAuthor = (token)
+    ? await getCommitAuthor(build)
+    : null;
 
   const text = (isWorking)
     ? `Build \`${build.id}\` started`
@@ -65,17 +75,15 @@ module.exports.createSlackMessage = (build) => {
   const message = {
     text,
     mrkdwn: true,
-    attachments: [
-      {
-        color: STATUS_COLOR[build.status] || DEFAULT_COLOR,
-        title: 'Build logs',
-        title_link: build.logUrl,
-        fields,
-        footer: 'Google Cloud Container Builder',
-        footer_icon: 'https://ssl.gstatic.com/pantheon/images/containerregistry/container_registry_color.png',
-        ts: timestamp,
-      },
-    ],
+    attachments: [{
+      color: STATUS_COLOR[build.status] || DEFAULT_COLOR,
+      title: 'Build logs',
+      title_link: build.logUrl,
+      fields,
+      footer: 'Google Cloud Container Builder',
+      footer_icon: 'https://ssl.gstatic.com/pantheon/images/containerregistry/container_registry_color.png',
+      ts: timestamp,
+    }],
   };
 
   // Add source information to the message.
@@ -90,11 +98,18 @@ module.exports.createSlackMessage = (build) => {
       title: 'Branch',
       value: build.source.repoSource.branchName,
     });
+
+    if (token) {
+      message.attachments[0].fields.push({
+        title: 'Commit Author',
+        value: commitAuthor,
+      });
+    }
   }
 
   // Add images to the message.
   const images = build.images || [];
-  for (let i = 0, len = images.length; i < len; i++) {
+  for (let i = 0, len = images.length; i < len; i += 1) {
     message.attachments[0].fields.push({
       title: 'Image',
       value: images[i],
